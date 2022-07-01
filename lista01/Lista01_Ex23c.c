99d20656e558098e4f0af1e91ac1fb01541fde78
jpaiva31 <106032726+jpaiva31@users.noreply.github.com>
2022-07-01 16:34:35 -0300

    Add files via upload

---
+++ lista01/Lista01_Ex23c.c
@@ -0,0 +1,13 @@
+/*Num dia de sol, você deseja medir a altura de um prédio, porém, a trena não é suficientemente longa. Assumindo que seja possível
+medir sua sombra e a do prédio no chão, e que você lembre da sua altura, faça um algoritmo para ler os dados necessários e calcular a
+altura do prédio. */
+#include <stdio.h>
+
+int main(){
+    float a,b,c,conta;
+    printf("Digite, respectivamente, sua altura, a altura da sua sombra e a altura da sombra do predio\n");
+    scanf("%f %f %f",&a,&b,&c);
+    conta=(c*a)/b;
+    printf("A altura do predio eh %f \n",conta);
+return 0;
+}
