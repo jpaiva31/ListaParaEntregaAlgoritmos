a7001263a335b42b985949ad830c90704e2580e1
jpaiva31 <106032726+jpaiva31@users.noreply.github.com>
2022-07-01 15:49:31 -0300

    Add files via upload

---
+++ lista01/Lista01_Ex18.c
@@ -0,0 +1,13 @@
+/*.A granja Frangotech possui um controle automatizado de cada frango da sua produção. No pé direito do
+frango há um anel com um chip de identificação; no pé esquerdo são dois anéis para indicar o tipo de alimento que ele deve consumir.
+Sabendo que o anel com chip custa R$4,00 e o anel de alimento custa R$3,50, faça um algoritmo para calcular o gasto total da granja
+ para marcar todos os seus frangos. */
+#include <stdio.h>
+
+int main(){
+    float a;
+    printf("Digite a quantidade de frangos da granja\n");
+    scanf("%f",&a);
+    printf("Voce gastara %f reais\n",a*11);
+return 0;
+}
