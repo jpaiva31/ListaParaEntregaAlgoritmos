62d787cc9ed745f2fb29ecc4aab716158e22d344
jpaiva31 <106032726+jpaiva31@users.noreply.github.com>
2022-07-01 16:42:49 -0300

    Add files via upload

---
+++ lista01/Lista01_Ex25.c
@@ -0,0 +1,10 @@
+/*Calcule o volume de uma caixa d'água cilíndrica*/
+#include <stdio.h>
+
+int main(){
+    float a,b;
+    printf("Digite, respectivamente, a altura do cilindro e sua base\n");
+    scanf("%f %f",&a, &b);
+    printf("O volume do cilindro eh %.2f \n",a*b);
+return 0;
+}
