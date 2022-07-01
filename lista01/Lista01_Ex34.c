9467dd3aabd95a8b4d12c70a6aeee36da545371f
jpaiva31 <106032726+jpaiva31@users.noreply.github.com>
2022-07-01 17:12:02 -0300

    Add files via upload

---
+++ lista01/Lista01_Ex34.c
@@ -0,0 +1,10 @@
+/*Faça um algoritmo que calcule e mostre a área de um quadrado. Sabe-se que: A = lado * lado; */
+#include <stdio.h>
+
+int main(){
+    float a;
+    printf("Digite o tamanho do lado do quadrado\n");
+    scanf("%f", &a);
+    printf("A area do quadrado eh %.2f\n",a*2);
+return 0;
+}
