b48623c889efc86a1110a7807ae1e08b4584b42b
jpaiva31 <106032726+jpaiva31@users.noreply.github.com>
2022-07-05 14:14:08 -0300

    Update Lista02_Ex04.c

--- Lista02/Lista02_Ex04.c
+++ Lista02/Lista02_Ex04.c
@@ -1,11 +1,11 @@
-/*Encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado.*/
+/*Encontrar o dobro de um nÃºmero caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado.*/
 
 #include <stdio.h>
 
 int main()
 {
     int a;
-    printf("Digite um numeros:\n");
+    printf("Digite um numero:\n");
     scanf("%d", &a);
     if(a<0)printf("%d\n", a*3);
     else printf("%d\n", a*2);
