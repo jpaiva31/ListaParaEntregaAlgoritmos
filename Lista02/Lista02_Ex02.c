13b6c38161650247c4fdc43ac0be57cfa1aa2058
jpaiva31 <106032726+jpaiva31@users.noreply.github.com>
2022-07-05 14:08:19 -0300

    Add files via upload

---
+++ Lista02/Lista02_Ex02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a;
+    printf("Digite um numero:\n");
+    scanf("%d", &a);
+    if(a%2==0)printf("esse numero eh par\n");
+    else printf("esse numero eh impar\n");
+
+    return 0;
+}
